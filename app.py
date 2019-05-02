fd860aadd03b134f0ef6c6f14d4c55f24996089f
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-05-02 11:02:57 +0100

    create route for history page and history.html

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+app.config['TESTING'] = True
+
 @app.route('/')
 def exceptedArrivals():
   return render_template('home.html')
@@ -11,4 +15,4 @@ def history():
   return render_template('history.html')
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run()
