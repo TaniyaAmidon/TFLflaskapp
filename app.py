bdc9bffb5986af454cd9456658143eea5cf0ecea
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-05-02 10:09:40 +0100

    create templates folder and add home.html

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
-app = Flast(__name__)
+app = Flask(__name__)
 
 @app.route('/')
-  def exceptedArrivals():
-    return render_template('home.html')
+def exceptedArrivals():
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
-  app.run(debug = True)
+  app.run(debug=True)
