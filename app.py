1b414d6d3437061b79140db15a1f5eff071f5452
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-05-02 09:51:08 +0100

    create home route and home.html

--- app.py
+++ app.py
@@ -2,5 +2,10 @@ from flask import Flask, render_template
 
 app = Flast(__name__)
 
+@app.route('/')
+  def exceptedArrivals():
+    return render_template('home.html')
+
+
 if __name__ == '__main__':
   app.run(debug = True)
