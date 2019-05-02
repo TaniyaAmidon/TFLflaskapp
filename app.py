2649eceec10915b4c8e26e98e96b1bdbfdc30265
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-05-02 10:16:51 +0100

    create layout.html

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def exceptedArrivals():
   return render_template('home.html')
 
+@app.route('/history')
+def history():
+  return render_template('history.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
