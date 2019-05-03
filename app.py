2d3411a87530588ae9d34585df98a0caeb5d814c
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-05-03 11:29:55 +0100

    add error handling

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 import requests
 import json
-import utils
+import db
 from dateutil import parser
 from pytz import timezone
 from datetime import datetime
+from flask import jsonify
 
 app = Flask(__name__)
 
@@ -12,14 +13,40 @@ app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 app.config['TESTING'] = True
 
-con = utils.connect_to_db()
-
+con = db.connect_to_db()
 cur = con.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS history (id SERIAL PRIMARY KEY, expectedArrival timestamptz, time_timestamp timestamptz)")
 con.commit()
 # cur.close()
 # con.close()
 
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+@app.route('/404')
+def get_foo():
+    raise InvalidUsage('This view is gone', status_code=410)
 
 @app.route('/')
 def display():
--- db.py
+++ db.py
@@ -2,3 +2,5 @@ import psycopg2
 
 def connect_to_db():
     return psycopg2.connect(dbname='tfl', user='taniyaamidon', host='localhost', password='password')
+
+
