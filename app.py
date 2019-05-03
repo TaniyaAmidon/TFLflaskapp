ae666c7c29abf2015c00a0aaccd0e0ba02724e6e
TaniyaAmidon <harshanitaniya@yahoo.co.uk>
2019-05-03 10:54:48 +0100

    create connect to db function

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 import requests
-import psycopg2
 import json
-
+import utils
+from dateutil import parser
+from pytz import timezone
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -10,29 +12,63 @@ app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 app.config['TESTING'] = True
 
-con = psycopg2.connect(dbname='flaskapp', user='taniyaamidon', host='localhost', password='password')
+con = utils.connect_to_db()
 
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS history (id serial PRIMARY KEY, expectedArrival varchar, timestamp varchar)")
-cur.execute("INSERT INTO history VALUES ( 1, 'value1', 'value2')")
-  #cur.execute("INSERT history(expectedArrival, timestamp VALUES jsonResponse[0]['timestamp'], jsonResponse[0]['expectedArrival'] )")
+cur.execute("CREATE TABLE IF NOT EXISTS history (id SERIAL PRIMARY KEY, expectedArrival timestamptz, time_timestamp timestamptz)")
 con.commit()
-cur.close()
-con.close()
-
+# cur.close()
+# con.close()
 
 
 @app.route('/')
 def display():
   r = requests.get('https://api.tfl.gov.uk/StopPoint/490009333W/arrivals').content
   jsonResponse = json.loads(r.decode('utf-8'))
-  return render_template('home.html', data=jsonResponse)
+
+  data = []
+  for item in jsonResponse:
+    london_tz = timezone('Europe/London')
+    dt = parser.parse(jsonResponse[0]['timestamp'])
+    tt = london_tz.normalize(dt.astimezone(london_tz))
+    timenow = tt.strftime('%H:%M:%S')
+    dt2 = parser.parse(item['expectedArrival'])
+    tt2 = london_tz.normalize(dt2.astimezone(london_tz))
+    mins = dt2 - dt
+    data.append(
+      {
+        "route": item['lineName'],
+        "stop": item['stationName'],
+        "destination": item['destinationName'],
+        "expectedArrival": round(mins.seconds/60),
+        "towards": item['towards']
+      }
+      )
+    data[::-1]
+
+    cur.execute("INSERT INTO history (expectedArrival, time_timestamp ) VALUES (TIMESTAMP '" + item['expectedArrival'] + "', TIMESTAMP '" + item['timestamp'] + "')")
+    con.commit()
+    # cur.close()
+    # con.close()
+  return render_template('home.html', data=data, tx=timenow)
+
 
 
 
 @app.route('/history')
 def history():
-  return render_template('history.html')
+  cur.execute("SELECT * FROM history")
+  items = cur.fetchall()
+  info = []
+  for row in items:
+    info.append(
+    {
+      "Id": row[0],
+      "expectedArrival": (row[1]).strftime('%H:%M:%S'),
+      "time_timestamp": (row[2]).strftime('%d:%m:%Y') + " / " +  (row[2]).strftime('%H:%M:%S')
+    }
+    )
+  return render_template('history.html', d=info)
 
 
 if __name__ == '__main__':
